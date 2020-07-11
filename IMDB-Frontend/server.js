10ee64e7078705b636b543eb04998048254852f5
Vipin <vipin.s0106@gmail.com>
2020-07-11 20:39:25 +0530

    Code Development completed

--- IMDB-Frontend/server.js
+++ IMDB-Frontend/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist/imdb-frontend'));
+app.use(express.static(__dirname+'/dist/IMDB-Frontend'));
 
 //PathLocationStrategy
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/imdb-frontend/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/IMDB-Frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
